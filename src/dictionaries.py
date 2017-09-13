eb51fb36f60ce2d275508c22dadd758e18ffe039
maiconfz <maiconfz@localhost>
2017-09-13 00:25:36 -0300

    Add dictionaries.py

---
+++ src/dictionaries.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    names = {
+        "A":["Adriana", "Ana"],
+        "M":["Maicon", "Marlon", "Marcelly"]
+    }
+    
+    print(names["A"])
+    print(names["M"])
+    print(names["A"][0])
+    print(names["M"][0])
